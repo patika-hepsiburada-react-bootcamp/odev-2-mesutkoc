8a4a976a0398b2968a1c7500bf322cee120a77b5
mesutkoc <mestkoc34@gmail.com>
2021-10-03 00:25:10 +0300

    Revert "hangman game"
    This reverts commit f1265cb619d1ba95d86321c8385ab15550f8eda2.

--- hangman/src/files/game-start.js
+++
@@ -1,6 +0,0 @@
-const words = ["HEPSIBURADA", "PATIKA", "REACTJS", "BOOTCAMP"];
-
-export default function defineWord() {
-  const word = Math.floor(Math.random() * words.length);
-  return words[word];
-}
